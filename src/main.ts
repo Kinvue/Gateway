dbe7e50667396327ded3326ba0f1392998b55307
illya-klus <illyaklus2@gmail.com>
2026-05-25 01:10:29 +0300

    [IA-65] add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configureService = app.get(ConfigService);
+  const port = configureService.getOrThrow<number>('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
