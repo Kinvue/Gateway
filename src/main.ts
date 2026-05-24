51e60ae2eae472a463d9c928afbdb11b915bf84b
illya-klus <illyaklus2@gmail.com>
2026-05-25 01:42:06 +0300

    [IA-67] add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
 
   const configureService = app.get(ConfigService);
   const port = configureService.getOrThrow<number>('PORT');
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    })
+  )
 
   await app.listen(port);
   logger.log(`Gateway started on port ${port}`);
